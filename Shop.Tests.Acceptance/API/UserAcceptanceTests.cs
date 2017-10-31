696b49b3fc46abad40aa85a5cae6b213383ecdeb
andrey.leskov <andrey.leskov@solomoto.com>
2017-10-31 22:03:31 +0300

    small refactoring

--- Shop.Tests.Acceptance/API/UserAcceptanceTests.cs
+++ Shop.Tests.Acceptance/API/UserAcceptanceTests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Shop.ReadModel.Context;
 using Shop.Web.Controllers;
 using Shop.Web.Controllers.Domain;
@@ -24,11 +20,10 @@ namespace Shop.Tests.Acceptance.API {
         {
             _shopTestContext = context;
             _testOutputHelper = output;
-
         }
 
         [Fact]
-        public async Task User_can_be_created()
+        public async Task User_can_be_created_and_it_can_be_fetched()
         {
             var data = new RegistrationViewModel(){Email="testUser@test.com", Password="testPass1;", FirstName = "testFirstName",LastName = "testLastName"};
             var userCreated = await _shopTestContext.Client.Post<UserCreatedViewModel>(Routes.Api.User.Controller + Routes.Api.User.Create, data);
@@ -40,28 +35,6 @@ namespace Shop.Tests.Acceptance.API {
             var userReadModel = await _shopTestContext.Client.Get<UserViewModel>(Routes.Api.User.Controller + userCreated.UserId);
            
             userReadModel.Login.ShouldEqual(data.Email);
-        }
-    }
-
-    public static class HttpClientTestExtensions
-    {
-        public static async Task<TResponse> Post<TResponse>(this HttpClient client,string route, object data)
-        {
-            var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(route, content);
-
-            response.EnsureSuccessStatusCode();
-            response.StatusCode.ShouldEqual(HttpStatusCode.OK);
-
-            return (TResponse)JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync(), typeof(TResponse));
-        }
-
-        public static async Task<TResponse> Get<TResponse>(this HttpClient client, string route)
-        {
-            var response = await client.GetAsync(route);
-            response.EnsureSuccessStatusCode();
-            response.StatusCode.ShouldEqual(HttpStatusCode.OK);
-            return (TResponse)JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync(), typeof(TResponse));
         }
     }
 }
